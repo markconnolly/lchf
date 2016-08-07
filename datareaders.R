5f7a0b90718f0bfd62e882ac4b3f2bb6f2b388ff
markconnolly <mark_connolly@acm.org>
2016-08-07 07:50:39 -0400

    notes for experiment added

--- datareaders.R
+++ datareaders.R
@@ -18,3 +18,31 @@ rbind.read.csvs <- function(fnames, ...) {
   
   return(do.call('rbind', list_of_data_frames))
 }
+
+# Predictors
+#     HR heart rate
+#     VO₂ max
+#     height
+#     weight
+#     fat mass
+#     lean mass
+#     fat:lean
+#     gender
+#     age
+#     RPE
+
+# Responses
+# FA oxidation rate
+# body composition (body fat percentage)
+# mood
+# metabolic indices
+#     ME (metabolic efficiency) point (switch from FA to CHO)
+#     RER (respiratory exchange ratio) proxy for ME point (hyp: reached at higher HR)
+#     65% VO₂  max is peak fat burning
+#     RPE (Borg Rating Perceived Exertion) (hyp: increase at each stage)
+#     body composition (hyp: fat:lean mass declines) 
+#     VO₂  max
+#     mood return to baseline
+
+# Repeated measures one-way anova
+# Tukey's HSD 
